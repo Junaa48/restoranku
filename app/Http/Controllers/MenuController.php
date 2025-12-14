b2260984a62203ac059fc369217a87bafbac4281
zyruxpie <arjunagustrinator123@gmail.com>
2025-12-14 11:27:47 +0800

    Add MenuController and views for menu, cart, and checkout functionality

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class MenuController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tableNumber = $request->query('meja');
+        if ($tableNumber) {
+            Session::put('tableNumber', $tableNumber);
+            Session::put('cart', $cart);
+        }
+
+        $items = Item::where('is_active', 1)->orderBy('name', 'asc')->get();
+
+        return view('customer.menu', compact('items', 'tableNumber'));
+    }
+
+    public function cart()
+    {
+        $cart =Session::get('cart');
+        return view('customer.cart');
+    }
+
+    public function addToCart(Request $request)
+    {
+        $menuId = $request->input('id');
+        $menu = Item::find($menuId);
+
+        $cart = Session::get('cart', []);
+
+        if (!$menu) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Menu tidak ditemukan'
+            ]);
+        }
+
+        if(isset($cart[$menuId])){
+            $cart[$menuId]['qty'] += 1;
+        } else {
+            $cart[$menuId] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'imge' => $menu->img,
+                'qty' => 1
+            ];
+        }
+
+        Session::put('cart', $cart);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil menambahkan ke keranjang',
+            'cart' => $cart
+        ]);
+    }
+}
