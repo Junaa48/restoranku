a26c7d03c5bac7919219bf584e5affb607f831b1
zyruxpie <arjunagustrinator123@gmail.com>
2026-03-23 20:08:35 +0800

    Add: enhance cart and checkout pages with header and dynamic pricing display

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -1,6 +1,16 @@
 @extends('customer.layouts.master')
 
 @section('content')
+    <!-- Single Page Header start -->
+    <div class="container-fluid page-header py-5">
+        <h1 class="text-center text-white display-6">Keranjang</h1>
+        <ol class="breadcrumb justify-content-center mb-0">
+            <li class="breadcrumb-item active text-primary">Silahkan periksa pesanan anda</li>
+        </ol>
+    </div>
+    <!-- Single Page Header End -->
+
+v   
     <div class="container-fluid py-5">
         <div class="container py-5">
             @if (session('success'))
